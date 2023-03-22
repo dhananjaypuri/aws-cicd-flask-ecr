38ac3d762f1df62c08283c109ffe4b81aa1fa9c9
dhananjaypuri <dhananjay.puri@gmail.com>
2023-03-22 13:09:46 +0000

    Modified wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,11 +5,11 @@ app = Flask(__name__);
 
 @app.route('/')
 def home():
-    return f"This is running on {socket.gethostname()}<br/><br/><h1>This is Home !!!!! <h1/>";
+    return f"This is running on {socket.gethostname()}<br/><br/><h1>This is Home Page !!!!! <h1/>";
 
 @app.route('/contact')
 def contact():
-    return f"This is running on {socket.gethostname()}<br/><br/><h1>This is Contact !!!!! <h1/>";
+    return f"This is running on {socket.gethostname()}<br/><br/><h1>This is Contact Page !!!!! <h1/>";
 
 if __name__ == "__main__":
     app.run(debug=True);
