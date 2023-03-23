e6cc833bc0426be130b7615bb805e9d13863ac41
dhananjaypuri <50763591+dhananjaypuri@users.noreply.github.com>
2023-03-23 22:40:08 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,7 @@ app = Flask(__name__);
 
 @app.route('/')
 def home():
-    return f"This is running on {socket.gethostname()}<br/><br/><h1>This is Home Page deployed from codepipeline ..!!!!! <h1/>";
+    return f"This is running on {socket.gethostname()}<br/><br/><h1>This is Home Page deployed from codepipeline !!!!! <h1/>";
 
 @app.route('/contact')
 def contact():
