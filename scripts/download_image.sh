fbe927189676c83d8036496063af40b0182c44ef
dhananjaypuri <dhananjay.puri@gmail.com>
2023-03-24 12:14:11 +0000

    Modified script download image

--- scripts/download_image.sh
+++ scripts/download_image.sh
@@ -2,5 +2,5 @@
 
 password="080916090676.dkr.ecr.us-east-1.amazonaws.com"
 IMAGE_NAME="demo-flask-app:latest"
-
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $password
 docker pull $password/$IMAGE_NAME
